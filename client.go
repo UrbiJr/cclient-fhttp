062ebe3a42d315a3a0b2141bf9d102e4f5438bde
Federico Urbinelli <federico.urbinelli@gmail.com>
2021-08-14 14:02:04 +0200

    added allowredirects option

--- client.go
+++ client.go
@@ -9,19 +9,41 @@ import (
 	utls "github.com/refraction-networking/utls"
 )
 
-func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+func NewClient(clientHello utls.ClientHelloID, allowRedirects bool, proxyUrl ...string) (http.Client, error) {
 	jar, _ := cookiejar.New(nil)
 	if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
 		dialer, err := newConnectDialer(proxyUrl[0])
 		if err != nil {
-			return http.Client{Jar: jar}, err
+			if !allowRedirects {
+				return http.Client{Jar: jar, CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				}}, err
+			} else {
+				return http.Client{Jar: jar}, err
+			}
 		}
-		return http.Client{
-			Transport: newRoundTripper(clientHello, dialer), Jar: jar,
-		}, nil
+		if !allowRedirects {
+			return http.Client{
+				Transport: newRoundTripper(clientHello, dialer), Jar: jar, CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}, nil
+		} else {
+			return http.Client{
+				Transport: newRoundTripper(clientHello, dialer), Jar: jar,
+			}, nil
+		}
 	} else {
-		return http.Client{
-			Transport: newRoundTripper(clientHello, proxy.Direct), Jar: jar,
-		}, nil
+		if !allowRedirects {
+			return http.Client{
+				Transport: newRoundTripper(clientHello, proxy.Direct), Jar: jar, CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}, nil
+		} else {
+			return http.Client{
+				Transport: newRoundTripper(clientHello, proxy.Direct), Jar: jar,
+			}, nil
+		}
 	}
 }
