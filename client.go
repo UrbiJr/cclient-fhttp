bb84d1216d6f9e7f6a8ecba2fde2e0c7c195bf05
Federico Urbinelli <federico.urbinelli@gmail.com>
2021-08-14 13:49:08 +0200

    add cookiejar to newclient

--- client.go
+++ client.go
@@ -1,6 +1,8 @@
 package cclient
 
 import (
+	"github.com/useflyent/fhttp/cookiejar"
+
 	http "github.com/useflyent/fhttp"
 	"golang.org/x/net/proxy"
 
@@ -8,17 +10,18 @@ import (
 )
 
 func NewClient(clientHello utls.ClientHelloID, proxyUrl ...string) (http.Client, error) {
+	jar, _ := cookiejar.New(nil)
 	if len(proxyUrl) > 0 && len(proxyUrl[0]) > 0 {
 		dialer, err := newConnectDialer(proxyUrl[0])
 		if err != nil {
-			return http.Client{}, err
+			return http.Client{Jar: jar}, err
 		}
 		return http.Client{
-			Transport: newRoundTripper(clientHello, dialer),
+			Transport: newRoundTripper(clientHello, dialer), Jar: jar,
 		}, nil
 	} else {
 		return http.Client{
-			Transport: newRoundTripper(clientHello, proxy.Direct),
+			Transport: newRoundTripper(clientHello, proxy.Direct), Jar: jar,
 		}, nil
 	}
 }
